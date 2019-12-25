b2fadd06aab99c801ab897fd579e07e1c77ef9b9
Taro <jiuzhemedingle1346@gmail.com>
2019-12-25 21:27:26 +0900

    retest ci

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             ]) : [],
         ],
 
-        'circle_test' => [
+        'circleci' => [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
